bbddc836f9731c63d6117d7e16820382092774a9
Muhammed Noushad <mammumuhammed606@gmail.com>
2025-06-18 11:17:57 +0530

    feat: add Hero and NavBar components with styling

---
+++ src/components/heroSection/Hero.jsx
@@ -0,0 +1,15 @@
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+  return (
+    <section className={styles.heroSection}>
+      <h4 className={styles.title}>DISCOVER OUR PRODUCTS</h4>
+      <p className={styles.description}>
+        Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus
+        scelerisque. Dolor integer scelerisque nibh amet mi ut elementum dolor.
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
